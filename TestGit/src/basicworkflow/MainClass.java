bf3149171e2bdf93e0856566a934ca04a0156e34
Alex <alex.olivieri81@gmail.com>
2018-03-08 20:57:26 +0100

    User Changed Parameters

--- TestGit/src/basicworkflow/MainClass.java
+++ TestGit/src/basicworkflow/MainClass.java
@@ -3,7 +3,7 @@ package basicworkflow;
 public class MainClass {
 
 	public static void main(String[] args) {
-		TestGit myFirstTestGit = new TestGitUpdate("My New Parameters");
+		TestGit myFirstTestGit = new TestGitUpdate("User Changed Parameters");
 		System.out.println(myFirstTestGit.returnTheString());
 	}
 }
