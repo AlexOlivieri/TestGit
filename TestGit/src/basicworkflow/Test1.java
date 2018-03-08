bb392672963221a03dd20907350ceb35aeaa812d
alexcarm.olivieri <alexcarm.olivieri@WH57469.hevs.ch>
2018-03-08 09:17:37 +0100

    Added two methods to make it executable and to print the String

--- TestGit/src/basicworkflow/Test1.java
+++ TestGit/src/basicworkflow/Test1.java
@@ -7,5 +7,14 @@ public class Test1 {
 	public Test1() {
 		this.test = "Here my test";
 	}
+	
+	public static void main(String[] args) {
+		Test1 myTest = new Test1();
+		System.out.println(myTest.getTestName());
+	}
+	
+	public String getTestName() {
+		return this.test;
+	}
 
 }
