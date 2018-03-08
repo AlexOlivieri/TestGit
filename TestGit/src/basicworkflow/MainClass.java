4e2ef5067262d9b895aec31459477da5ec6db600
alexcarm.olivieri <alexcarm.olivieri@WH57469.hevs.ch>
2018-03-08 20:56:48 +0100

    Owner Changed Parameters

--- TestGit/src/basicworkflow/MainClass.java
+++ TestGit/src/basicworkflow/MainClass.java
@@ -3,7 +3,7 @@ package basicworkflow;
 public class MainClass {
 
 	public static void main(String[] args) {
-		TestGit myFirstTestGit = new TestGitUpdate("My New Parameters");
+		TestGit myFirstTestGit = new TestGitUpdate("Owner Changed Parameters");
 		System.out.println(myFirstTestGit.returnTheString());
 	}
 }
