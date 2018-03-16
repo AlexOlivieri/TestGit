6d7aa395b592080f61d878f5315b107c37e862ba
MyTestGitAccount <mindfulness.fitness@gmail.com>
2018-03-16 14:14:33 +0100

    Conflict Solved

--- TestGit/src/basicworkflow/MainClass.java
+++ TestGit/src/basicworkflow/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		TestGit myFirstTestGit = new TestGitUpdate("Owner Changed Parameters");
-
+		
 		System.out.println(myFirstTestGit.returnTheString());
 	}
 }
